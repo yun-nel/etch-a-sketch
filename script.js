8a6d1a6e8f970bcef89f90daa3b90390d052110a
jaeyun <d65971@gmail.com>
2023-03-22 07:01:24 +1030

    feat: add html skeleton for grid slider

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const sketchContainer = document.querySelector(".grid-container");
 // Creates 256 divs in the DOM.
 for (let i = 0; i < 256; i++) {
     const newDiv = document.createElement("div");
-    newDiv.classList.add("grid")
+    newDiv.classList.add("grid");
     sketchContainer.appendChild(newDiv);
 }
 

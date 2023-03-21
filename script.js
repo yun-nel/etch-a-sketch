5453f7b6c1ea0316d056e4df8165558cfbe595f7
jaeyun <d65971@gmail.com>
2023-03-22 06:32:33 +1030

    feat: add sketching in random RGB

--- script.js
+++ script.js
@@ -8,21 +8,32 @@ for (let i = 0; i < 256; i++) {
     sketchContainer.appendChild(newDiv);
 }
 
-const grid = document.querySelectorAll(".grid");
-
-// Adds functionality for user to sketch in black.
-function sketchBlack() {
-    grid.forEach(function(element) {
-        element.addEventListener("mouseover", function() {
-            element.style.backgroundColor = "black"
-        });
-    });
-}
-
+// Returns a randomised RGB value. 
 function getRandomRGB() {
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     return `rgb(${red}, ${green}, ${blue})`;
 }
-sketchBlack()
+
+const grid = document.querySelectorAll(".grid");
+
+// Adds functionality for user to sketch in black.
+function sketchBlack() {
+    grid.forEach(function(element) {
+        element.addEventListener("mouseover", function() {
+            element.style.backgroundColor = "black";
+        });
+    });
+}
+
+// Adds functionality for user to sketch in random RGB values.
+function sketchRandom() {
+    grid.forEach(function(element) {
+        element.addEventListener("mouseover", function() {
+            element.style.backgroundColor = getRandomRGB()
+        });
+    });
+}
+
+sketchRandom()

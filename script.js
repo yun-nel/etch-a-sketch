5f19161a0c7d5adb369a24945250c673ac0607d0
jaeyun <d65971@gmail.com>
2023-03-22 06:14:37 +1030

    feat: add sketching

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const sketchContainer = document.querySelector(".grid-container");
 
+
+// Creates 256 divs in the DOM.
 for (let i = 0; i < 256; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("grid")
     sketchContainer.appendChild(newDiv);
 }
+
+const grid = document.querySelectorAll(".grid");
+
+grid.forEach(function(element) {
+    element.addEventListener("mouseover", function() {
+        element.style.backgroundColor = "black"
+    });
+});
